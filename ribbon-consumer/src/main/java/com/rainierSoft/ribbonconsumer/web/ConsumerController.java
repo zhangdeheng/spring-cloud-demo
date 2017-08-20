77c4574a898d29f6d24fe8871d7fabd40c9c3f05
zhangdeheng <1600976604@qq.com>
2017-08-20 13:29:11 +0800

    加入请求合并、缓存
    添加缓存功能
    添加请求合并功能

--- ribbon-consumer/src/main/java/com/rainierSoft/ribbonconsumer/web/ConsumerController.java
+++ ribbon-consumer/src/main/java/com/rainierSoft/ribbonconsumer/web/ConsumerController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rainierSoft.ribbonconsumer.service.HelloService;
+
 
 @RestController
 public class ConsumerController {
---
+++ ribbon-consumer/src/main/java/com/rainierSoft/ribbonconsumer/web/UserCommand.java
@@ -0,0 +1,9 @@
+package com.rainierSoft.ribbonconsumer.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserCommand{
+
+
+}
