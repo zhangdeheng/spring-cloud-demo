91d08cb3f02201eb9ddd640d93a116d78de4a4e3
zhangdeheng <1600976604@qq.com>
2017-10-29 19:30:42 +0800

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# spring-cloud
-微服务搭建
--- ribbon-consumer/src/main/java/com/rainierSoft/ribbonconsumer/web/UserCommand.java
+++
@@ -1,9 +0,0 @@
-package com.rainierSoft.ribbonconsumer.web;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserCommand{
-
-
-}
