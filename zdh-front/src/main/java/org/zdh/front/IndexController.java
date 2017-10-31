4ef06bd8f866ceed8a2ccabcb7b308262cf5fb87
zhangdeheng <1600976604@qq.com>
2017-10-31 20:24:25 +0800

    添加商品

--- zdh-front/src/main/java/org/zdh/front/IndexController.java
+++ zdh-front/src/main/java/org/zdh/front/IndexController.java
@@ -1,18 +1,21 @@
 package org.zdh.front;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.zdh.common.pageutil.Page;
+import org.zdh.goodsmgr.service.IGoodsService;
 import org.zdh.goodsmgr.vo.GoodsModel;
 import org.zdh.goodsmgr.vo.GoodsQueryModel;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
-//	@Autowired
-//	private IGoodsService igs = null;
+	@Autowired
+	private IGoodsService igs = null;
 //	@Autowired
 //	private ICartService ics = null;
 //	@Autowired
@@ -24,25 +27,25 @@ public class IndexController {
 //	
 	@RequestMapping(value="/toIndex",method=RequestMethod.GET)
 	public String toIndex(Model model){
-//		GoodsQueryModel gqm = new GoodsQueryModel();
-//		gqm.getPage().setPageShow(100);
-//
-//		Page<GoodsModel> page = igs.getByConditionPage(gqm);
-//		
-//		model.addAttribute("page",page);
+		GoodsQueryModel gqm = new GoodsQueryModel();
+		gqm.getPage().setPageShow(100);
+
+		Page<GoodsModel> page = igs.getByConditionPage(gqm);
+		
+		model.addAttribute("page",page);
 		
 		
 		return "index";
 	}
-//	
-//	@RequestMapping(value="/toGoodsDesc/{goodsUuid}",method=RequestMethod.GET)
-//	public String toGoodsDesc(Model model,@PathVariable("goodsUuid")int goodsUuid){
-//		GoodsModel gm = igs.getByUuid(goodsUuid);
-//		
-//		model.addAttribute("m",gm);
-//		return "goods/desc";
-//	}
-//	
+	
+	@RequestMapping(value="/toGoodsDesc/{goodsUuid}",method=RequestMethod.GET)
+	public String toGoodsDesc(Model model,@PathVariable("goodsUuid")int goodsUuid){
+		GoodsModel gm = igs.getByUuid(goodsUuid);
+		
+		model.addAttribute("m",gm);
+		return "goods/desc";
+	}
+	
 //	@RequestMapping(value="/addToCart/{goodsUuid}",method=RequestMethod.GET)
 //	public String addToCart(Model model,@PathVariable("goodsUuid")int goodsUuid,@CookieValue("MyLogin")String myLogin){
 //		int customerUuid = Integer.parseInt( myLogin.split(",")[0]);
